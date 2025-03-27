2b6ee2ed95b2cfe8a8b14cc2502e98d5161c1d51
Ashtonrb <78123522+Ashtonrb@users.noreply.github.com>
2025-03-27 16:03:43 -0500

    Updated firmware

--- Arduino Files/ece445PlatformIO/src/main.cpp
+++ Arduino Files/ece445PlatformIO/src/main.cpp
@@ -1,49 +1,143 @@
 #include <Arduino.h>
+#include <math.h>
 
+//define pins
 const int buttonPin = 25;
 const int xPin = 26;
 const int yPin = 27;
 
-int xMin, xMax, yMin, yMax;
-bool lastButtonState = HIGH;
+//define global variables
+float xMin, xMax, yMin, yMax;
+
+bool lastButton = LOW;
 int pressCount = 0;
 
-float lowerBoundDeg = 0;
-float upperBoundDeg = 0;
+float lower = 0;
+float upper = 0;
+
+float alpha = 0.3;
+float threshold = 2;
+float xfiltered = 0;
+float yfiltered = 0;
+float xnext = 0;
+float ynext = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(buttonPin, INPUT_PULLUP); // assuming button is wired to GND
+  pinMode(buttonPin, INPUT); 
 
+  //Get initial values
   xMin = analogRead(xPin);
   xMax = analogRead(xPin);
   yMin = analogRead(yPin);
   yMax = analogRead(yPin);
+  xnext = analogRead(xPin);
+  ynext = analogRead(yPin);
+  xfiltered = analogRead(xPin);
+  yfiltered = analogRead(yPin);
 }
 
 void loop() {
+  //Get current readings
   float x = analogRead(xPin);
   float y = analogRead(yPin);
+  xnext = alpha * x + (1 - alpha) * xfiltered;
+  ynext = alpha * y + (1 - alpha) * yfiltered;
 
-  // Track min/max for normalization
-  if (x < xMin) xMin = x;
-  if (x > xMax) xMax = x;
-  if (y < yMin) yMin = y;
-  if (y > yMax) yMax = y;
+  if (abs(xnext - xfiltered) > threshold) {
+    xfiltered = xnext;
+  }
 
+  if (abs(ynext - yfiltered) > threshold) {
+    yfiltered = ynext;
+  }
+  //Update min/max's
+  if (xfiltered < xMin) xMin = xfiltered;
+  if (xfiltered > xMax) xMax = xfiltered;
+  if (yfiltered < yMin) yMin = yfiltered;
+  if (yfiltered > yMax) yMax = yfiltered;
+
+  //Define and get local variables - Normalize x and y
   float xMid = (xMax + xMin) / 2.0;
   float xHalf = (xMax - xMin) / 2.0;
-  float xNorm = (x - xMid) / xHalf;
+  float xNorm = (xfiltered - xMid) / xHalf;
 
   float yMid = (yMax + yMin) / 2.0;
   float yHalf = (yMax - yMin) / 2.0;
-  float yNorm = (y - yMid) / yHalf;
+  float yNorm = (yfiltered - yMid) / yHalf;
 
-  float w = atan2(yNorm, xNorm) * 180.0 / PI;  // w is in degrees, [-180, 180]
+  //Get angle, map to [0, 360)
+  float w = (atan2(yNorm, xNorm) * 180.0 / PI) + 180;  // w is in degrees, [-180, 180]
 
-  Serial.println("x: " + String(xNorm, 2) +
-                 " y: " + String(yNorm, 2) +
-                 " w: " + String(w, 1));
+  //Get bounds
+  const unsigned long debounceDelay = 20; // ms
+  unsigned long lastDebounceTime = 0;
+  
+  bool button = digitalRead(buttonPin);
+  static bool lastButton = LOW;
+  static bool debouncedState = LOW;
+  
+  unsigned long currentTime = millis();
+  
+  // Check if the input changed
+  if (button != lastButton) {
+    lastDebounceTime = currentTime;  // reset debounce timer
+  }
+  
+  if ((currentTime - lastDebounceTime) > debounceDelay) {
+    // Button has been stable long enough
+    if (button != debouncedState) {
+      debouncedState = button;
+  
+      if (debouncedState == HIGH) {
+        // Rising edge: clean button press
+        if (pressCount == 0) {
+          lower = w;
+          pressCount++;
+          Serial.println("Lower bound set");
+        } else if (pressCount == 1) {
+          upper = w;
+          pressCount = 0;
+          Serial.println("Upper bound set");
+        }
+      }
+    }
+  }
+  
+  lastButton = button;
+  
+  //Map (lower, upper) -> (0, 1)
+  //First we need to shift our read in phase w such that w = 0 if it is lower. Assuming the delta(lower, upper) < 360, this will also fix the issue where lower > upper due to phase reset.
+  float tolerance = 5.0;
+
+  float range = fmod(upper - lower + 360.0, 360.0);
+  float delta = fmod(w - lower + 360.0 + tolerance, 360.0);
+  float hallf;
+
+  float mapped;
+
+  if (delta > range) {
+    mapped = 1.0;
+  } else {
+    mapped = delta / range;
+  }
+
+
+  // Serial.println("x: " + String(x, 2) +
+  //                " y: " + String(y, 2) +
+  //                " filtered x: " + String(xfiltered, 2) +
+  //                " filtered y: " + String(yfiltered, 2) +
+  //                " w: " + String(w, 2));
+
+  Serial.println("button: " + String(button) +
+                 " last button: " + String(lastButton) +
+                 " press count: " + String(pressCount) +
+                 " w: " + String(w, 1) +
+                 " lower: " + String(lower, 2) +
+                 " upper: " + String(upper, 2) +
+                 " range: " + String(range, 2) +
+                 " delta: " + String(delta, 2) +                 
+                 " mapped: " + String(mapped, 2));
 
   delay(20);
 }
