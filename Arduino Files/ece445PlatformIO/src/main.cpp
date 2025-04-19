fc42eebe329b861cb36130653d88088ab4e29dc8
Ashtonrb <78123522+Ashtonrb@users.noreply.github.com>
2025-04-19 17:45:25 -0500

    Upped code to 5 floats, included joystick, and added float value override capabilities

--- Arduino Files/ece445PlatformIO/src/main.cpp
+++ Arduino Files/ece445PlatformIO/src/main.cpp
@@ -1,30 +1,37 @@
 #include <Arduino.h>
 #include <math.h>
 
-//define pins
-const int xPin = 26;
-const int yPin = 27;
+const int NUM_SENSORS = 5;
 
-//define global variables
-float xMin, xMax, yMin, yMax;
-float w;
-bool lastButton = LOW;
-int pressCount = 0;
+// Sensor pins
+const int xPins[NUM_SENSORS] = {26, 32, 34, 36, 38};
+const int yPins[NUM_SENSORS] = {27, 33, 35, 39, 37};
 
-float lower = 0;
-float upper = 0;
+// Joystick pins
+const int joystickXPin = A0;
+const int joystickYPin = A1;
+
+// Buttons
+const int NUM_BUTTONS = 2;
+const int buttonPins[NUM_BUTTONS] = {25, 33};
+const unsigned long debounceDelay = 50;
 
 float alpha = 0.3;
-float threshold = 2;
-float xfiltered = 0;
-float yfiltered = 0;
-float xnext = 0;
-float ynext = 0;
+float threshold = 2.0;
+float tolerance = 5.0;
 
-//New button stuff
-const int NUM_BUTTONS = 2;
-const int buttonPins[NUM_BUTTONS] = {33, 25}; // Replace with your actual pins
-const unsigned long debounceDelay = 50;
+struct SensorPair {
+  int xPin;
+  int yPin;
+  float xMin, xMax, yMin, yMax;
+  float xFiltered, yFiltered;
+  float lower, upper;
+  int pressCount;
+  float w;
+  float mapped;
+};
+
+SensorPair sensors[NUM_SENSORS];
 
 struct Button {
   bool currentState;
@@ -35,79 +42,90 @@ struct Button {
 
 Button buttons[NUM_BUTTONS];
 
+// Override mode
+bool useOverride = false;
+const float offsets[NUM_SENSORS] = {0.0, 0.2, 0.4, 0.6, 0.8};
+float overrideValues[NUM_SENSORS];
+const unsigned long cycleDuration = 1000;
+unsigned long startTime;
+
+int activeIndex = 0;
+unsigned long button1PressTime = 0;
+bool button1Held = false;
+
+// Joystick smoothing
+float joyXFiltered = 0;
+float joyYFiltered = 0;
+float joystickAlpha = 0.3;
+float joystickThreshold = 5.0;
+
+void generateOverrideValues() {
+  unsigned long elapsed = (millis() - startTime) % cycleDuration;
+  float rampValue = (float)elapsed / cycleDuration;
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    float offsetValue = rampValue + offsets[i];
+    if (offsetValue > 1.0) offsetValue -= 1.0;
+    overrideValues[i] = offsetValue;
+  }
+}
+
 void handleButtonPress(int buttonIndex) {
   switch (buttonIndex) {
     case 0:
-      // Example for button 0
-      if (pressCount == 0) {
-        lower = w;
-        pressCount++;
-      } else if (pressCount == 1) {
-        upper = w;
-        pressCount = 0;
+      if (sensors[activeIndex].pressCount == 0) {
+        sensors[activeIndex].lower = sensors[activeIndex].w;
+        sensors[activeIndex].pressCount++;
+      } else if (sensors[activeIndex].pressCount == 1) {
+        sensors[activeIndex].upper = sensors[activeIndex].w;
+        sensors[activeIndex].pressCount = 0;
       }
       break;
     case 1:
-      break;
-    case 2:
-      break;
-    case 3:
+      activeIndex = (activeIndex + 1) % NUM_SENSORS;
+      Serial.println("Active Index: " + String(activeIndex));
       break;
   }
 }
 
 void setup() {
   Serial.begin(115200);
+
   for (int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(buttonPins[i], INPUT);
     buttons[i] = {LOW, LOW, LOW, 0};
   }
-  //Set initial values
-  xMin = analogRead(xPin);
-  xMax = analogRead(xPin);
-  yMin = analogRead(yPin);
-  yMax = analogRead(yPin);
-  xnext = analogRead(xPin);
-  ynext = analogRead(yPin);
-  xfiltered = analogRead(xPin);
-  yfiltered = analogRead(yPin);
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    sensors[i].xPin = xPins[i];
+    sensors[i].yPin = yPins[i];
+
+    int xInit = analogRead(sensors[i].xPin);
+    int yInit = analogRead(sensors[i].yPin);
+
+    sensors[i].xMin = sensors[i].xMax = xInit;
+    sensors[i].yMin = sensors[i].yMax = yInit;
+
+    sensors[i].xFiltered = xInit;
+    sensors[i].yFiltered = yInit;
+
+    sensors[i].lower = 0;
+    sensors[i].upper = 0;
+    sensors[i].pressCount = 0;
+    sensors[i].w = 0;
+    sensors[i].mapped = 0;
+  }
+
+  joyXFiltered = analogRead(joystickXPin);
+  joyYFiltered = analogRead(joystickYPin);
+
+  startTime = millis();
 }
 
 void loop() {
-  //Get current readings
-  float x = analogRead(xPin);
-  float y = analogRead(yPin);
-  xnext = alpha * x + (1 - alpha) * xfiltered;
-  ynext = alpha * y + (1 - alpha) * yfiltered;
-
-  if (abs(xnext - xfiltered) > threshold) {
-    xfiltered = xnext;
-  }
-
-  if (abs(ynext - yfiltered) > threshold) {
-    yfiltered = ynext;
-  }
-  //Update min/max's
-  if (xfiltered < xMin) xMin = xfiltered;
-  if (xfiltered > xMax) xMax = xfiltered;
-  if (yfiltered < yMin) yMin = yfiltered;
-  if (yfiltered > yMax) yMax = yfiltered;
-
-  //Define and get local variables - Normalize x and y
-  float xMid = (xMax + xMin) / 2.0;
-  float xHalf = (xMax - xMin) / 2.0;
-  float xNorm = (xfiltered - xMid) / xHalf;
-
-  float yMid = (yMax + yMin) / 2.0;
-  float yHalf = (yMax - yMin) / 2.0;
-  float yNorm = (yfiltered - yMid) / yHalf;
-
-  //Get angle, map to [0, 360)
-  w = (atan2(yNorm, xNorm) * 180.0 / PI) + 180;  // w is in degrees, [-180, 180]
-
-  //Button stuff
   unsigned long currentTime = millis();
 
+  // --- Button Debouncing ---
   for (int i = 0; i < NUM_BUTTONS; i++) {
     buttons[i].currentState = digitalRead(buttonPins[i]);
 
@@ -120,47 +138,92 @@ void loop() {
         buttons[i].debouncedState = buttons[i].currentState;
 
         if (buttons[i].debouncedState == HIGH) {
-          handleButtonPress(i);  // handle press based on index
+          handleButtonPress(i);
+          if (i == 1) {
+            button1PressTime = currentTime;
+            button1Held = true;
+          }
         }
       }
     }
 
+    if (i == 1 && button1Held && buttons[i].debouncedState == LOW) {
+      if (currentTime - button1PressTime > 1000) {
+        useOverride = !useOverride;
+        Serial.println(useOverride ? "Override Mode: ON" : "Override Mode: OFF");
+      }
+      button1Held = false;
+    }
+
     buttons[i].lastState = buttons[i].currentState;
   }
-  
-  //Map (lower, upper) -> (0, 1)
-  //First we need to shift our read in phase w such that w = 0 if it is lower. Assuming the delta(lower, upper) < 360, this will also fix the issue where lower > upper due to phase reset.
-  float tolerance = 5.0;
 
-  float range = fmod(upper - lower + 360.0, 360.0);
-  float delta = fmod(w - lower + 360.0 + tolerance, 360.0);
-  float hallf;
+  // --- Sensor Processing ---
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    SensorPair &s = sensors[i];
 
-  float mapped;
+    float x = analogRead(s.xPin);
+    float y = analogRead(s.yPin);
 
-  if (delta > range) {
-    mapped = 1.0;
-  } else {
-    mapped = delta / range;
+    float xNext = alpha * x + (1 - alpha) * s.xFiltered;
+    float yNext = alpha * y + (1 - alpha) * s.yFiltered;
+
+    if (abs(xNext - s.xFiltered) > threshold) s.xFiltered = xNext;
+    if (abs(yNext - s.yFiltered) > threshold) s.yFiltered = yNext;
+
+    if (s.xFiltered < s.xMin) s.xMin = s.xFiltered;
+    if (s.xFiltered > s.xMax) s.xMax = s.xFiltered;
+    if (s.yFiltered < s.yMin) s.yMin = s.yFiltered;
+    if (s.yFiltered > s.yMax) s.yMax = s.yFiltered;
+
+    float xMid = (s.xMax + s.xMin) / 2.0;
+    float xHalf = (s.xMax - s.xMin) / 2.0;
+    float xNorm = (s.xFiltered - xMid) / xHalf;
+
+    float yMid = (s.yMax + s.yMin) / 2.0;
+    float yHalf = (s.yMax - s.yMin) / 2.0;
+    float yNorm = (s.yFiltered - yMid) / yHalf;
+
+    s.w = atan2(yNorm, xNorm) * 180.0 / PI + 180.0;
+
+    float range = fmod(s.upper - s.lower + 360.0, 360.0);
+    float delta = fmod(s.w - s.lower + 360.0 + tolerance, 360.0);
+    s.mapped = (delta > range) ? 1.0 : delta / range;
   }
 
+  // --- Override ---
+  generateOverrideValues();
 
-  // Serial.println("x: " + String(x, 2) +
-  //                " y: " + String(y, 2) +
-  //                " filtered x: " + String(xfiltered, 2) +
-  //                " filtered y: " + String(yfiltered, 2) +
-  //                " w: " + String(w, 2));
+  // --- Joystick Smoothing ---
+  int rawJoyX = analogRead(joystickXPin);
+  int rawJoyY = analogRead(joystickYPin);
 
-  Serial.println("button 0: " + String(buttons[0].currentState) +
-                 " button 1: " + String(buttons[1].currentState) +
-                 " press count: " + String(pressCount) +
-                 " w: " + String(w, 1) +
-                 " lower: " + String(lower, 2) +
-                 " upper: " + String(upper, 2) +
-                 " range: " + String(range, 2) +
-                 " delta: " + String(delta, 2) +                 
-                 " mapped: " + String(mapped, 2));
+  float joyXNext = joystickAlpha * rawJoyX + (1 - joystickAlpha) * joyXFiltered;
+  float joyYNext = joystickAlpha * rawJoyY + (1 - joystickAlpha) * joyYFiltered;
+
+  if (abs(joyXNext - joyXFiltered) > joystickThreshold) joyXFiltered = joyXNext;
+  if (abs(joyYNext - joyYFiltered) > joystickThreshold) joyYFiltered = joyYNext;
+
+  int joyX = (int)joyXFiltered;
+  int joyY = (int)joyYFiltered;
+
+  // --- Output for Unity ---
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    float val = useOverride ? overrideValues[i] : sensors[i].mapped;
+    Serial.print(val, 3);
+    if (i < NUM_SENSORS - 1) Serial.print(", ");
+  }
+
+  Serial.print(" | ");
+  Serial.print(joyX);
+  Serial.print(" | ");
+  Serial.print(joyY);
+  Serial.print(" | ");
+  Serial.print(useOverride ? "1" : "0");
+  Serial.print(" | ");
+  Serial.print(buttons[0].debouncedState ? "1" : "0");
+  Serial.print(" | ");
+  Serial.println(buttons[1].debouncedState ? "1" : "0");
 
   delay(20);
 }
-
